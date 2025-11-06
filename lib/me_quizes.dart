6e1d086890a348c72320369e40b58ad0414f8410
Mikołaj Sawicz <sawicz01@gmail.com>
2025-11-06 20:07:14 +0100

    Revert "merge"
    This reverts commit f02679ce071adbb59d9f72cac23ee3b6f574dfb4, reversing
    changes made to 94a86cbd10004ef8d201fcccf411ce9158a09c40.

--- lib/me_quizes.dart
+++ lib/me_quizes.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_quizarena/models/QuizMetadata';
-import 'package:flutter_quizarena/quiz_edit.dart';
 import 'package:flutter_quizarena/repositories/QuizRepository.dart';
 
 class MeQuizes extends StatefulWidget {
@@ -14,95 +13,6 @@ class _MeQuizesState extends State<MeQuizes> {
   final QuizRepository quizRepository = QuizRepository();
   final String? currentUserId = FirebaseAuth.instance.currentUser?.uid;
 
-
- void showQuizCreateDialog() {
-  final TextEditingController nameController = TextEditingController();
-  const List<String> accessOptions = ['public', 'private', 'friendsOnly'];
-
-  String selectedAccessType = accessOptions.first; 
-  
-  final BuildContext safeContext = context;
-  String statusMessage = '';
-
-  showDialog(
-    context: safeContext,
-    builder: (BuildContext dialogContext) {
-      return StatefulBuilder(
-        builder: (BuildContext context, StateSetter setState) {
-          return AlertDialog(
-            title: const Text('Create Quiz'),
-            content: SingleChildScrollView(
-              child: ListBody(
-                children: <Widget>[
-                  TextField(
-                    controller: nameController,
-                    decoration: const InputDecoration(
-                      labelText: 'Name of Quiz',
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  DropdownButtonFormField<String>(
-                    decoration: const InputDecoration(
-                      labelText: 'Access type',
-                      border: OutlineInputBorder(),
-                    ),
-                    value: selectedAccessType,
-                    items: accessOptions.map((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    onChanged: (String? newValue) {
-                      if (newValue != null) {
-                        setState(() { 
-                          selectedAccessType = newValue;
-                        });
-                      }
-                    },
-                  ),
-                ],
-              ),
-            ),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () => Navigator.of(dialogContext).pop(),
-                child: const Text('Cancel'),
-              ),
-              FilledButton(
-                onPressed: () async {
-                  final quizName = nameController.text.trim();
-                  if (quizName.isEmpty) {
-                     statusMessage = 'Name cannot be empty';
-                  } else {
-                    Navigator.of(dialogContext).pop(); 
-                    try {
-                      await quizRepository.createQuiz(name: nameController.text, accessType: selectedAccessType);
-                      statusMessage = 'Quiz "$quizName" created!'; 
-                    } catch (e) {
-                      statusMessage = 'Error: ${e.toString()}';
-                    }
-                  }
-                  
-                  if(safeContext.mounted) {
-                    ScaffoldMessenger.of(safeContext).showSnackBar(
-                      SnackBar(
-                        content: Text(statusMessage), 
-                        duration: const Duration(seconds: 3),
-                      ),
-                    );
-                  }
-                },
-                child: const Text('Create Quiz'),
-              ),
-            ],
-          );
-        },
-      );
-    },
-  );
-}
-
   @override
   Widget build(BuildContext context) {
     if (currentUserId == null) {
@@ -112,7 +22,7 @@ class _MeQuizesState extends State<MeQuizes> {
     return Scaffold(
       appBar: AppBar(title: const Text('My Quizes')),
       body: StreamBuilder<List<QuizMetadata>>(
-        stream: quizRepository.getAvailableQuizzesStream2(), 
+        stream: quizRepository.getAvailableQuizzesStream(), 
         
         builder: (context, snapshot) {
           
@@ -149,17 +59,11 @@ class _MeQuizesState extends State<MeQuizes> {
                     style: const TextStyle(fontWeight: FontWeight.bold),
                   ),
                   subtitle: Text(
-                    'Access: ${quiz.accessType}, Owner: ${quiz.ownerName}',
-            
+                    'Access: ${quiz.accessType}',
                   ),
                   trailing: const Icon(Icons.arrow_forward_ios, size: 16),
                   onTap: () {
-                    Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => QuizEdit(quizMetadata: quiz),
-                    ),
-                  );
+                    print('Selected quiz for editon: ${quiz.name}');
                   },
                 ),
               );
@@ -168,8 +72,7 @@ class _MeQuizesState extends State<MeQuizes> {
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {showQuizCreateDialog();
-
+        onPressed: () {
         },
         child: const Icon(Icons.add),
       ),
--- lib/repositories/QuizRepository.dart
+++ lib/repositories/QuizRepository.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_quizarena/models/QuizMetadata';
-import 'package:flutter_quizarena/models/QuizQustion';
 import 'package:flutter_quizarena/models/User';
 import 'package:flutter_quizarena/repositories/FriendRepository.dart';
 import 'package:flutter_quizarena/repositories/UserRepository.dart';
@@ -14,49 +13,6 @@ class QuizRepository {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final UserRepository _userRepository = UserRepository();
   final FriendRepository _friendRepository = FriendRepository();
-
-  Stream<List<QuizQuestion>> getQuestionsStream(String quizId) {
-    return _firestore
-        .collection('quiz')
-        .doc(quizId)
-        .collection('questions')
-        .snapshots()
-        .map((snapshot) {
-          return snapshot.docs.map((doc) {
-            return QuizQuestion.fromFirestore(doc.data(), doc.id);
-          }).toList();
-        });
-  }
-
-  Future<void> createQuiz({
-    required String name,
-    required String accessType,
-  }) async {
-    if(_auth.currentUser == null) {
-      throw Exception("Unauthorized user, please reauth");
-    }
-    String ownerId = _auth.currentUser!.uid;
-    if (name.trim().isEmpty) {
-      throw Exception("Quiz name cannot be empty");
-    }
-    if (!['public', 'private', 'friendsOnly'].contains(accessType)) {
-      throw Exception("Error: AccessType must be public', 'private', 'friendsOnly");
-    }
-
-    final newQuizData = {
-      'ownerID': ownerId,
-      'name': name.trim(),
-      'accessType': accessType,
-    };
-
-    try {
-      await _firestore.collection('quiz').add(newQuizData);
-    } on FirebaseException catch (e) {
-      throw Exception('Firebase Error: ${e.message}');
-    } catch (e) {
-      throw Exception('Unknown Error: $e');
-    }
-  }
 
   Stream<List<QuizMetadata>> getAvailableQuizzesStream() {
     final userQuizzes = _firestore
@@ -94,7 +50,7 @@ class QuizRepository {
       final streams = chunks.map((chunk) {
         return _firestore
             .collection('quiz')
-            .where('accessType', isEqualTo: 'friendsOnly')
+            .where('accessType', isEqualTo: 'friendOnly')
             .where('ownerID', whereIn: chunk)
             .snapshots()
             .map((snapshot) => snapshot.docs
@@ -125,102 +81,6 @@ class QuizRepository {
     );
       
   }
-
-  Stream<List<QuizMetadata>> getAvailableQuizzesStream2() {
-    final userQuizzes = _firestore
-        .collection('quiz')
-        .where('accessType', isEqualTo: 'private')
-        .where('ownerID', isEqualTo: AuthService().currentUser!.uid)
-        .snapshots()
-        .map((snapshot) {
-          return snapshot.docs.map((doc) {
-            return QuizMetadata.fromFirestore(doc.data(), doc.id);
-          }).toList();
-        });
-
-    final publicQuizzes = _firestore
-        .collection('quiz')
-        .where('accessType', isEqualTo: 'public')
-        .snapshots()
-        .map((snapshot) {
-          return snapshot.docs.map((doc) {
-            return QuizMetadata.fromFirestore(doc.data(), doc.id);
-          }).toList();
-        });
-
-    final friendUidsStream = _friendRepository
-        .streamFriendList(_auth.currentUser!.uid)
-        .map((userList) => userList.map((user) => user.uid).toList());
-
-    final friendQuizzesStream = friendUidsStream.switchMap((friendUids) {
-      if (friendUids.isEmpty) {
-        return Stream.value(<QuizMetadata>[]);
-      }
-
-      final chunks = _chunkList(friendUids, 10);
-      
-      final streams = chunks.map((chunk) {
-        return _firestore
-            .collection('quiz')
-            .where('accessType', isEqualTo: 'friendsOnly')
-            .where('ownerID', whereIn: chunk)
-            .snapshots()
-            .map((snapshot) => snapshot.docs
-                .map((doc) => QuizMetadata.fromFirestore(doc.data(), doc.id))
-                .toList());
-      }).toList();
-      
-      return streams.isNotEmpty
-          ? Rx.combineLatestList<List<QuizMetadata>>(streams)
-             .map((listOfLists) => listOfLists.expand((list) => list).toList())
-          : Stream.value(<QuizMetadata>[]);
-    });
-
-      final combinedStream = Rx.combineLatest3(
-        publicQuizzes,
-        userQuizzes,
-        friendQuizzesStream,
-        (List<QuizMetadata> pubQuizzes, List<QuizMetadata> userQuizzes, List<QuizMetadata> friendQuizzes) {
-
-          final allQuizzes = [...pubQuizzes, ...friendQuizzes, ...userQuizzes];
-
-          final uniqueQuizzes = <String, QuizMetadata>{};
-          for (var quiz in allQuizzes) {
-            uniqueQuizzes[quiz.id] = quiz;
-          }
-          return uniqueQuizzes.values.toList();
-        },
-      );
-      
-      return combinedStream.switchMap((quizzes) {
-        if (quizzes.isEmpty) {
-          return Stream.value(<QuizMetadata>[]);
-        }
-        return Stream.fromFuture(_fetchOwnerNames(quizzes));
-      });
-  }
-
-
-  Future<List<QuizMetadata>> _fetchOwnerNames(List<QuizMetadata> quizzes) async {
-  if (quizzes.isEmpty) return [];
-
-  final Set<String> ownerIds = quizzes.map((q) => q.ownerId).toSet();
-
-  late final Map<String, UserModel> ownersMap;
-  try {
-    ownersMap = await _userRepository.getUsersMap(ownerIds); 
-  } catch (e) {
-    print("Błąd pobierania danych właścicieli: $e");
-    ownersMap = {};
-  }
-
-  return quizzes.map((quiz) {
-    final ownerData = ownersMap[quiz.ownerId];
-    final ownerName = ownerData?.username ?? 'Unknown';
-
-    return quiz.copyWith(ownerName: ownerName); 
-  }).toList();
-}
 
   List<List<T>> _chunkList<T>(List<T> list, int chunkSize) {
     List<List<T>> chunks = [];
@@ -258,7 +118,7 @@ class QuizRepository {
   
   Stream<List<QuizMetadata>> getMyQuizzesStream(String? userId) {
 
-    userId = AuthService().currentUser!.uid;
+    userId ??= AuthService().currentUser!.uid;
     return _firestore
         .collection('quiz')
         .where('ownerId', isEqualTo: userId)
